41d3889d417191b428b41d24378b629bdde17eec
DavidPeng11 <pbdyx11@outlook.com>
2016-06-02 20:14:21 +0800

    make the sqrt&log working

--- Kernel/Kernel_Regression.py
+++ Kernel/Kernel_Regression.py
@@ -2,12 +2,6 @@ import gensim
 import codecs
 import numpy as np
 import math
-# # regression model
-# from sklearn.kernel_ridge import KernelRidge
-# from sklearn.svm import SVR
-# from sklearn.neighbors import KNeighborsRegressor
-# from sklearn.ensemble import RandomForestRegressor
-# from sklearn.linear_model import Ridge
 from sklearn.linear_model import LinearRegression
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import mean_absolute_error
@@ -25,16 +19,13 @@ print ('seed file loaded')
 known_word_valence = dict()
 # known_word_arousal = dict()
 for line in seed_file:
-    line1, line2 = line.split('\t', 2)[:-1]
+    line1, line2 = line.strip().split('\t', 2)[:-1]
     known_word_valence[line1] = float(line2)
     # known_word_arousal[line1] = float(line3)
 
 K_list = filter(lambda a: a in vocab_list, known_word_valence.keys())
 # Find the words which are match with the corpus
 Train_list, Test_list = train_test_split(K_list, test_size=0.3, random_state=0)
-# N_list=K_list[:1000]
-# # get the first 1000 words as the training set
-# L_list=K_list[1000:]
 
 x_train = []
 for i in Train_list:
@@ -43,9 +34,19 @@ for i in Train_list:
         d = model.similarity(i, j)
         # f=math.sqrt(d)
         # f=math.exp(d)
-        f=math.log(d)
-        v = known_word_valence[j]
-        x_train_row.append(f * v)
+        # if d>=0:
+        #     f=math.sqrt(d)
+        #     v = known_word_valence[i]
+        #     x_train_row.append(f * v)
+        # elif d<0:
+        #     d1=abs(d)
+        #     f=math.sqrt(d1)
+        #     v=known_word_valence[i]
+        #     x_train_row.append((-1)*f*v)
+        # f=math.exp(d)
+        f=d
+        v=known_word_valence[j]
+        x_train_row.append(f*v)
     x_train.append(list(x_train_row))
 
 x_train = np.array(x_train)
@@ -66,11 +67,21 @@ for i in Test_list:
         d = model.similarity(i, j)
         # f=math.sqrt(d)
         # f=math.exp(d)
-        f=math.log(d)
-        v = known_word_valence[j]
-        x_test_row.append(f * v)
+        # if d>=0:
+        #     f=math.sqrt(d)
+        #     v = known_word_valence[i]
+        #     x_test_row.append(f * v)
+        #
+        # elif d<0:
+        #     d1=abs(d)
+        #     f=math.sqrt(d1)
+        #     v= known_word_valence[i]
+        #     x_test_row.append((-1)*f*v)
+        # f=math.exp(d)
+        f=d
+        v=known_word_valence[i]
+        x_test_row.append(f*v)
     x_test.append(list(x_test_row))
-
 x_test = np.array(x_test)
 print ('x_test shape:', x_test.shape)
 
@@ -92,4 +103,4 @@ mae = mean_absolute_error(y_test, y_pred)
 print mae
 r = pearsonr(y_test, y_pred)[0]
 print r
-pass
+
--- Kernel/vector_regression.py
+++ Kernel/vector_regression.py
@@ -8,14 +8,17 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from scipy.stats import pearsonr
 
-def MakeArray(input_list, seed_value, model):
-    output_x=[]
-    output_y=[]
+
+def MakeArray(input_list, seed_value,  model):
+    output_x = []
+    output_y = []
+
     for i in input_list:
         output_x.append(model[i])
         output_y.append(seed_value[i])
-    output_x=np.array(output_x)
-    output_y=np.array(output_y)
+    output_x = np.array(output_x)
+    output_y = np.array(output_y)
+
     return output_x, output_y
 
 
@@ -24,46 +27,38 @@ if __name__ == '__main__':
     model = gensim.models.Word2Vec.load("C:\Pycharm\Projects\Word2Vec\wiki.zh.text.model")
     print ('model loaded')
 
-    seed_file=codecs.open("C:\Pycharm\Projects\Kernel\seed_sim.txt", 'r', 'utf8')
+    seed_file = codecs.open("C:\Pycharm\Projects\Kernel\seed_sim.txt", 'r', 'utf8')
     print ('seed file loaded')
 
-    model_list= list(model.vocab.keys())
+    model_list = list(model.vocab.keys())
 
-    seed_word_valence=dict()
+    seed_word_valence = dict()
+    # seed_word_arousal = dict()
     for line in seed_file:
-        line = line.split('\t',-1)
-        word_line=line[0]
-        valence_line=line[1]
-        # arousal_line=line[2]
-        seed_word_valence[word_line]=float(valence_line)
+        line = line.strip().split('\t', -1)
+        word_line = line[0]
+        valence_line = line[1]
+        arousal_line = line[2]
+        seed_word_valence[word_line] = float(valence_line)
+        # seed_word_arousal[word_line] = float(arousal_line)
 
-    match_list= filter(lambda a:a in model_list, seed_word_valence.keys())
+    match_list = filter(lambda a: a in model_list, seed_word_valence.keys())
 
-    train_list, test_list= train_test_split(match_list,test_size=0.4,random_state=0)
-    x_train,y_train=MakeArray(train_list, seed_word_valence, model)
-    print ("x_train's shape is %d "% x_train.shape)
-    print ("y_train's shape is %d "% y_train.shape)
+    train_list, test_list = train_test_split(match_list, test_size=0.3, random_state=0)
+    x_train, y_train = MakeArray(train_list, seed_word_valence, model)
 
-    x_test,y_test=MakeArray(test_list, seed_word_valence,model)
-    print ("x_test's shape is %d " % x_test.shape)
-    print ("y_test's shape is %d " % y_test.shape)
+
+    x_test, y_test = MakeArray(test_list, seed_word_valence, model)
+
 
     lr = LinearRegression()
     lr.fit(x_train, y_train)
 
-    y_predict=lr.predict(x_test)
+    y_predict = lr.predict(x_test)
 
     rmse = math.sqrt(mean_squared_error(y_test, y_predict))
-    print ('the RMSE is %f'%rmse)
+    print ('the RMSE is %s '% rmse)
     mae = mean_absolute_error(y_test, y_predict)
-    print ('the MAE is %f'%mae)
+    print ('the MAE is ', mae)
     r = pearsonr(y_test, y_predict)[0]
-    print ('the pearsonr is %f'%r)
-
-
-
-
-
-
-
-
+    print ('the pearsonr is' , r)
